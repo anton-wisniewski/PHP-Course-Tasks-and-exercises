eb9127666c5538332ecc6a688a4f89b5deb64f55
Anton Wiśniewski <wisniewski.anton01@gmail.com>
2024-07-15 16:44:16 +0200

    Add files via upload

---
+++ 05+variables.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num = 100; // deklaracj zmiennej
+        $num = 123;
+
+        echo("Wartość w zmiennej num to: $num ");
+
+        // Nieprawidłowe nazwy zmiennych
+        //$data value = 12;
+        //$data.value = 12;
+        //$5sum = 14;
+        //$sum-test = 14;
+
+        //$sydufgsuydfgu = 199; // zła praktyka
+
+
+        // Prawidłowe nazwy czyli identyfikatory
+        $data_value = 12;
+        $_4sum = 56;
+
+        $numStudentsInSchool = 300; // camelCase
+        $numstudentsinschool = 300; // zła praktyka
+
+
+
+        $topSpeed = 220;
+        $carBrand = "Ford";
+        $carModel = "Mustang";
+        $carProductionYear = 2004;
+        $carColor = "red";
+        
+    ?>
+</body>
+</html>
---
+++ 06+variables.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        /*
+        Napisz przykładowe zmienne opisujące wiadomość email np.:
+        - tytuł emaila
+        - treść maila
+        - odbiorca
+        - nadawca
+        - data wysyłki
+        Stosuj angielskie nazwy identyfikatorów oraz camelCase, przypisz
+        początkowe wartości do zmiennych.
+        */
+
+        $emailTitle = "Hello";
+        $emailContent = "Hello friend!";
+        $emailRecipient = "adam@example.com";
+        $emailSender = "kuba@example.com";
+        $emailDate = "2023.01.01";
+
+    ?>
+    
+</body>
+</html>
