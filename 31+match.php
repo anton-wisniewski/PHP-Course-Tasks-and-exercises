878e81ff299f606a513935efe8cef11b875b5729
Anton Wiśniewski <wisniewski.anton01@gmail.com>
2024-07-22 19:24:17 +0200

    Add files via upload

---
+++ 31+match.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+    
+
+
+
+    <?php  
+        
+        $num = 8;
+
+        $result = match($num) {
+            0 => "zero",
+            3 => "trzy",
+            8 => "osiem",
+            10 => "dziesięć",
+            default => "nieznana wartość"
+        };
+
+        echo($result);
+
+
+
+
+
+
+
+    ?>
+    
+</body>
+</html>
